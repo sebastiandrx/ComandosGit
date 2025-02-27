c2173750242ebd7e345fb48ab9a2259bc826ae07
sebastiandrx <sebastianduenas05@gmail.com>
2025-02-26 20:48:28 -0500

    Prueba1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Prueba comandos Git");
-        System.out.println("Prueba clonacion a sebastian, Jorge herrera :D");
+        System.out.println("Prueba del comando merge");
         }
     }
